64864d7d7b75ffb16f183576268789874a8635ef
chibaba0 <mikenyandoro0@gmail.com>
2020-09-30 13:02:06 +0200

    fries and burger

--- hungry.py
+++ hungry.py
@@ -2,5 +2,7 @@ hungry = input('Are you hungry: ')
 if hungry == "yes":
     print("eat samosa")
     print("eat pizza")
+    print("burger")
+    print("fries")
 else:
     print("do your homework")
